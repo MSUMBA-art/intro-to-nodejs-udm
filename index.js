dc1248adc3a9a891a86dba7e38a316ca6629e655
ALEXANDER <msumba83@gmail.com>
2021-11-04 07:07:07 +0200

    npm

---
+++ index.js
@@ -0,0 +1,11 @@
+// const superheroes = require("superheroes");
+
+// const mySuperheroes = superheroes.random();
+
+// console.log(mySuperheroes);
+
+const supervillains = require("supervillains");
+
+const mySupervillainsName = supervillains.random();
+
+console.log(mySupervillainsName);
