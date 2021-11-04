c3fd21b01faed55fe65a740002ad41bb204b1ed9
ALEXANDER <msumba83@gmail.com>
2021-11-04 16:28:15 +0200

    done

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
- const superheroes = require("superheroes");
-
-const mySuperheroes = superheroes.random();
-
-const supervillains = require('supervillains');
-=======
 // const superheroes = require("superheroes");
 
 // const mySuperheroes = superheroes.random();
@@ -12,12 +5,8 @@ const supervillains = require('supervillains');
 // console.log(mySuperheroes);
 
 const supervillains = require("supervillains");
->>>>>>> dc1248adc3a9a891a86dba7e38a316ca6629e655
 
 const mySupervillainsName = supervillains.random();
 
 console.log(mySupervillainsName);
-<<<<<<< HEAD
 console.log(mySuperheroes);
-=======
->>>>>>> dc1248adc3a9a891a86dba7e38a316ca6629e655
