a166245aa208363aa0c1cac40bb686f3916c477b
ALEXANDER <msumba83@gmail.com>
2021-11-04 16:25:53 +0200

    npm

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-const fs = require("fs");
+ const superheroes = require("superheroes");
 
-fs.copyFileSync("file1.txt", "file2.txt");
+const mySuperheroes = superheroes.random();
+
+const supervillains = require('supervillains');
+
+const mySupervillainsName = supervillains.random();
+
+console.log(mySupervillainsName);
+console.log(mySuperheroes);
